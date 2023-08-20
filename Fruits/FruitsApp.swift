fdb319a03c96358640a79852af16a4fdddca8595
mohammadgdz <mhd.gdz@gmail.com>
2023-08-20 20:42:40 +0330

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Mohammad on 2023-08-20.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
